03197a6626de93146c6fed16b26ed9f96392c1a9
AaronMorimoto <119596908+AaronMorimoto@users.noreply.github.com>
2023-02-01 12:21:10 +0000

    renew

--- sample.js
+++ sample.js
@@ -1,14 +1,14 @@
 // varによる再宣言
-//var nickname = "taro"
-//console.log(nickname)
-//var nickname = "ichiro"
-//console.log(nickname)
+var nickname = "taro"
+console.log(nickname)
+var nickname = "ichiro"
+console.log(nickname)
 
 // letによる再宣言
-let nickname = "taro"
-console.log(nickname)
-let nickname = "ichiro"
-console.log(nickname)
+//let nickname = "taro"
+//console.log(nickname)
+//let nickname = "ichiro"
+//console.log(nickname)
 
 // constによる再宣言
 //const nickname = "taro"
